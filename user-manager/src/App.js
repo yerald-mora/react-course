973f174cf3e2f0cc24977e97a9ecb17d7832c3a4
Yerald Mora <anton-gerard@hotmail.com>
2021-10-23 16:38:38 -0600

    Create Button component

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -2,9 +2,10 @@ import useForm from './Hooks/UseForm'
 import Input from './Components/Input'
 import Card from './Components/Card'
 import Container from './Components/Container'
+import Button from './Components/Button'
 
 function App(){
-  const[form,handleChange] = useForm({name:'', lastName:''})
+  const[form,handleChange] = useForm({name:'', lastName:'', email:''})
 
   console.log(form);
 
@@ -13,7 +14,9 @@ function App(){
       <Card>
         <form>
           <Input label='Name' name='name' placeholder='Name' value={form.name} onChange={handleChange}/>
-          <Input label='Last Name' name='lastName' placeholder='Last Name' value={form.name} onChange={handleChange}/>
+          <Input label='Last Name' name='lastName' placeholder='Last Name' value={form.lastName} onChange={handleChange}/>
+          <Input label='Email' name='email' placeholder='Email' value={form.email} onChange={handleChange}/>
+          <Button>Enviar</Button>
         </form>
       </Card>
     </Container>
